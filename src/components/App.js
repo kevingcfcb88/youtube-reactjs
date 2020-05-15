5f887dc62b5cfd7f954a545b1bb5226559c2423e
kevin.garay <kevingcfcb88@gmail.com>
2020-05-14 21:55:07 -0600

    Adding new component VideoDetail

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import youtube from '../api/youtube';
 import SearchBar from './SearchBar';
 import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
 class App extends React.Component{
   state = {videos:[], selectedVideo:null};
@@ -23,6 +24,7 @@ class App extends React.Component{
     return (
       <div className="ui container">
         <SearchBar onSearch={this.onSearch}/>
+        <VideoDetail video={this.state.selectedVideo}/>
         <VideoList videos={this.state.videos} onVideoSelect={this.onVideoSelect}/> 
       </div>
     );
